c1a75b4ce3848d264dfcacdc7917c403b60bff09
IanPC <iancaminhas25@hotmail.com>
2022-12-01 17:25:09 -0300

    Customizando o tratamento de Erros da aplicacao

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -8,6 +9,32 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+/* Middleware abaixo: interceptar todas as mensagens da
+aplicação para customizar a apresentação do erro no front-end.
+Em suma, vai capturar o erro gerado para ser tratado e apresentado
+na forma mais amigável.
+error: Error já é nativo do JavaScript. Não é necessário importar.
+O Middleware recebe 4 parâmetros:
+error: Error, request: Request, response: Response, next: NextFunction
+*/
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    //Se o error for uma instância da classe AppError(ou seja da própria aplicação), lança o status e o message.
+    //exemplos de erros: validação, autenticação, página não encontrada
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    //se o erro não for da aplicação, ou seja, um erro desconhecido
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () =>{
   console.log('server started on port 3333!');
