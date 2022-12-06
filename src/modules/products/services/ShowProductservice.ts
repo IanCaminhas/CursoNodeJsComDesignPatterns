4d75115127b1f6103722f99806f37d23e356c1ca
IanPC <iancaminhas25@hotmail.com>
2022-12-05 22:01:24 -0300

    Servico para atualizar produto

---
+++ src/modules/products/services/ShowProductservice.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/http/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    //vai retornar um unico produto
+    const product = productsRepository.findOne(id);
+    //Tratamento caso nao encontrar nada
+    if (!product) {
+      throw new AppError('Product not found. ');
+    }
+    return product;
+  }
+}
+
+export default ShowProductService;
