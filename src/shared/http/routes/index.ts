707328e9e74ef77497aa5798c74c155961b47c67
IanPC <iancaminhas25@hotmail.com>
2022-12-08 16:12:34 -0300

    Rotas de produtos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,18 @@
+import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
+/*
+  Quando acessar /products, ele vai acessar as rotas de productsRoutes
+  Exemplo: /products/id/-> :id foi defindo em products.routes.ts
+*/
+routes.use('/products', productsRouter);
+/*
+Rota de teste
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Dev!' });
 });
+*/
 
 export default routes;
