ba23eb11ad350836160adb63dc2d2767cf0c33a0
IanPC <iancaminhas25@hotmail.com>
2022-12-09 08:38:00 -0300

    Controller usuarios

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,25 @@
+import { Request, response, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUserService from '../services/ListUserService';
+
+export default class usersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUser = new ListUserService();
+    const users = listUser.execute();
+    return response.json(users);
+  }
+
+  public async create(request: Request): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
