feaa27144bbe965f2702387c7d4aa893368c4a11
IanPC <iancaminhas25@hotmail.com>
2022-12-10 08:32:22 -0300

    Controller de sessao de autenticacao

---
+++ src/modules/products/controllers/SessionsController.ts
@@ -0,0 +1,17 @@
+import CreateSessionsService from '@modules/users/services/CreateSessionService';
+import { Request, Response } from 'express';
+
+export default class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const createSession = new CreateSessionsService();
+
+    const user = await createSession.execute({
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
