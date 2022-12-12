cd27cb670aa44ac1680f24b11f4524904f226694
IanPC <iancaminhas25@hotmail.com>
2022-12-12 18:26:09 -0300

    Rota Estática para as Imagens de Avatar

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,6 +7,7 @@ import AppError from './errors/AppError';
 /*quando a aplicação subir, vai importar o typeorm\index.ts, vai chamar
 o metodo createConnection(). Por fim, create connection() vai importar as configurações de ormconfig.json*/
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 
 //importacao do celebrate para exibir todos os erros da aplicação
 import { errors } from 'celebrate';
@@ -15,6 +16,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//Quando for acessar a rota /files/nomeIMG da api, vai pegar o conteudo que esta no diretorio uploads. Aí vai subir no navegador.
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
