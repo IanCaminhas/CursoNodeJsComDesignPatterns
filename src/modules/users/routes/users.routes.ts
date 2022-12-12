adcafa137b8bec8385c2abf340e0f8afaddaea3b
IanPC <iancaminhas25@hotmail.com>
2022-12-12 18:14:19 -0300

    Rota de Upload de Avatar de Usuário

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/userAvatarController';
 import isAuthenticated from '../../../shared/middlewares/IsAuthenticated';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
 
+//middleware de envio de arquivo
+//definição do multer já com as configurações definidas.
+const upload = multer(uploadConfig);
+
+//isAuthenticated é o middleware de autenticação
 usersRouter.get('/', isAuthenticated, usersController.index);
 
 usersRouter.post(
@@ -18,6 +27,14 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+//permite a atualização de único dado(no caso é o avatar)
+//vai ser /users/avatar
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated, //usuario esta autenticado para fazer o upload do avatar ?
+  upload.single('avatar'), //single() significa o upload de único arquivo. avatar é o nome do campo lá na hora do insomnia
+  usersAvatarController.update,
 );
 
 export default usersRouter;
