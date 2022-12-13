c541dc417b5b7d1092247ddb9a020c2b832ad1c9
IanPC <iancaminhas25@hotmail.com>
2022-12-13 08:11:50 -0300

    Migration da tabela user_tokens

---
+++ src/shared/typeorm/migrations/1670928424720-CreateUserTokens.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTokens1670928424720 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'token',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'TokenUser',
+            referencedTableName: 'users', //nome da outra tabela
+            referencedColumnNames: ['id'], //nome do campo que referencio. Outra tabela
+            columnNames: ['user_id'], //coluna da tabela atual que sera referenciada
+            onDelete: 'CASCADE', //se o usuario que está na tabela users for deletado, o que acontece com os registros que tem relação com esse campo ?  Deleta o TokenUser. Pronto.
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_tokens');
+  }
+}
