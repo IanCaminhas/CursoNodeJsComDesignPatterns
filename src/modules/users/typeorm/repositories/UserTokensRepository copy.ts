0c996fa5326cebf5038fc155031dab9251598d50
IanPC <iancaminhas25@hotmail.com>
2022-12-13 08:38:05 -0300

    Repo. de tokens de usuarios

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository copy.ts
@@ -0,0 +1,28 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+class UserTokensRepository extends Repository<UserToken> {
+  //Pesquisar pelo token
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+
+    return userToken;
+  }
+  // Criar um token
+  public async generate(user_id: string): Promise<UserToken | undefined> {
+    const userToken = await this.create({
+      user_id,
+    });
+
+    await this.save(userToken);
+
+    return userToken;
+  }
+}
+
+export default UserTokensRepository;
