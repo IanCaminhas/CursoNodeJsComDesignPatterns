b0012b53a19a534a7aa7e4f944126c34b7191f47
IanPC <iancaminhas25@hotmail.com>
2022-12-23 16:23:49 -0300

    Entidade de Produtos em um Pedido de Compras

--- src/modules/orders/typeorm/entities/Order.ts
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -5,8 +5,10 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import OrdersProducts from './OrdersProducts';
 
 @Entity('orders')
 class Order {
@@ -14,8 +16,18 @@ class Order {
   id: string;
 
   @ManyToOne(() => Customers)
-  @JoinColumn({ name: 'customer_id' }) //nome da coluna que faz a referência
+  @JoinColumn({ name: 'customer_id' })
   customer: Customers;
+
+  //lembre: é um array pq é @OneToMany
+  /*
+    Toda que salvar uma Order, preciso salvar os OrdersProducts.
+    Como proceder o salvamento ? cascade: true
+  */
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true, //Toda vez que criar uma order, todos os OrdersProducts relacionados a essa order ja vao ser salvas automaticamente no bd
+  })
+  order_products: OrdersProducts[];
 
   @CreateDateColumn()
   created_at: Date;
